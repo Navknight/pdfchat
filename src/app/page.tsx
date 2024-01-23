805bf74efdf861c3ee986aa7b65c16d5c6e42fb1
Abhinav <abhi.gupta8802@gmail.com>
2024-01-23 16:27:27 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FileUpload from '@/components/fileUpload';
 import { Button } from '@/components/ui/button'
 import { UserButton, auth } from '@clerk/nextjs'
 import { LogIn } from 'lucide-react';
@@ -6,7 +7,7 @@ import Link from 'next/link';
 
 export default async function Home() {
 	const { userId } = await auth();
-	const isAtuh = !!userId;
+	const isAuth = !!userId;
 	return (
 		<div className='w-screen min-h-screen bg-gradient-to-b from-indigo-200 via-red-200 to-yellow-100'>
 			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -16,11 +17,11 @@ export default async function Home() {
 						<UserButton afterSignOutUrl='/' />
 					</div>
 					<div className="flex mt-2">
-						{isAtuh && <Button>Got to chats</Button>}
+						{isAuth && <Button>Got to chats</Button>}
 					</div>
 					<p className='max-w-xl mt-1 text-lg text-slate-600'>Join others in understanding AI.</p>
 					<div className="w-full mt-4">
-						{isAtuh ? (<h1>File Upload</h1>) : (
+						{isAuth ? (<FileUpload />) : (
 							<Link href="/sign-in">
 								<Button>Sign In to start 
 									<LogIn className='w-4 h-4 ml-2'></LogIn>
